e83cff5a71e8d0869f6e3d24e38e303567090d56
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-01-15 21:37:33 +0000

    Add primary constructor (& properties)

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,5 +1,9 @@
-class Person {
+class Person constructor(
+    var firstName: String = "john",
+    var lastName: String = "doe",
+    var age: Int = 18,
+) { // Primary constructor
     override fun toString(): String {
-        return "I'm a person"
+        return "$firstName $lastName - $age years old"
     }
 }
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,5 +1,23 @@
 fun main() {
-    val p = Person()
+    println("Person 1:")
+    val p1 = Person("hassan", "alaoui", 23)
+    println(p1)
 
-    println(p)
+    println("\nPerson 2:")
+    val p2 = Person()
+    println(p2)
+
+    println("\nPerson 3:")
+    val p3 = Person(firstName = "jane", age = 21)
+    println(p3)
+
+    println("\nPerson 4:")
+    val p4 = Person("hamid", "hassani")
+    println(p4)
+    p4.firstName = "hamida"
+    p4.age = 19
+    println("After: \n" +
+            "Firstname: ${p4.firstName}\n" +
+            "Lastname: ${p4.lastName}\n" +
+            "Age: ${p4.age}")
 }
