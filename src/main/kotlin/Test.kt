31c87e72e3f149de44357fd831cb98398694f9b4
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-01-18 22:46:31 +0000

    Nested and Inner classes

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,9 +1,30 @@
 fun main() {
-    var a = Account("hassan.alaoui", "123456")
-    var (l, p) = a
+    println("Base: ")
+    var o1 = Base()
+    var o2 = Base()
 
-    println("a1: $a")
+    o1.p = 2
+    o2.p = 5
 
-    println("l = $l")
-    println("p = $p")
+    o1.f()
+    o2.f()
+
+    println("\nNestedClass: ")
+
+    println("Anonymous object: ")
+    println(Base.NestedClass().pn) // ? Base.NestedClass() is an anonymous object
+    Base.NestedClass().pn += 3
+    println(Base.NestedClass().pn)
+
+    println("Named object: ")
+    var objOfNestedClass = Base.NestedClass()
+    println(objOfNestedClass.pn)
+    objOfNestedClass.pn *= 2
+    objOfNestedClass.fn()
+
+    println("\nInnerClass: ")
+
+    println(o1.InnerClass().pi)
+    o1.InnerClass().fi()
+    o2.InnerClass().fi()
 }
